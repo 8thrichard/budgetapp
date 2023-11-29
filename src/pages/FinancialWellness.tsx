f70eca14cfc10365e13f9f899a6abbd5fc1f6883
Richard Boamah <richboam8@gmail.com>
2023-11-28 20:42:34 -0600

    More info

--- src/pages/FinancialWellness.tsx
+++ src/pages/FinancialWellness.tsx
@@ -8,10 +8,19 @@ const FinancialWellness: React.FC = () => {
       <div>
         <h2>Reach your Financial goals one step at a time</h2>
         <p>Use Our list of important milestones and rules to achieve financial independence.</p>
+        <button>Get Started</button>
       </div>
-
       <div>
         <h1>More Content</h1>
+        <h3>Key Topics</h3>
+      <ul>
+        <li>1. Budgeting and Saving</li>
+        <li>2. Investing Strategies</li>
+        <li>3. Debt Management</li>
+        <li>4. Emergency Funds</li>
+        <li>5. Retirement Planning</li>
+      </ul>
+
       </div>
     </div>    
   );
