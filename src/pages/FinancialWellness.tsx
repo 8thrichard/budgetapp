8f89d3aea2cca36133458f75d3be09948a35c8b6
Richard Boamah <richboam8@gmail.com>
2023-11-28 20:33:10 -0600

    Init code sent. Need some more ideas to complete the finance page

--- src/pages/FinancialWellness.tsx
+++ src/pages/FinancialWellness.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import '../styles/Financial.css';
 
 
 const FinancialWellness: React.FC = () => {
   return (
-    <div>
-      <h2> Financial Wellness</h2>
-    </div>
+    <div className="finance-main">
+      <div>
+        <h2>Reach your Financial goals one step at a time</h2>
+        <p>Use Our list of important milestones and rules to achieve financial independence.</p>
+      </div>
+
+      <div>
+        <h1>More Content</h1>
+      </div>
+    </div>    
   );
 };
 
