3a8ec7b143d12e09b735f122a20b113c08c46543
Richard Boamah <richboam8@gmail.com>
2023-12-06 09:33:13 -0600

    Completed

--- src/pages/Saving.tsx
+++ src/pages/Saving.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import StockData from './StockData';
+import '../styles/saving.css';
 
 const Saving: React.FC = () => {
   return (
     <div>
-      <StockData/>
-      <h1>Savings</h1>
-      {/* Add your content for the home page here */}
+      <div className="savings-container">
+        <h2>Saving Challenges</h2>
+        <div className="video-container">
+          <iframe
+            title="Savings Video"
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/cp7vqImQl18"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <p>
+          Explore the challenges and strategies for achieving your savings goals in
+          this insightful video.
+        </p>
+      </div>
     </div>
   );
 };
